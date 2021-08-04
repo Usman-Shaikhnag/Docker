8713a9907cf887a7795834de72a7e076574b062e
k1h1a1n <affykhan789678@gmail.com>
2021-08-04 20:48:27 +0530

    try

--- modules/quality_control/__init__.py
+++ modules/quality_control/__init__.py
@@ -4,21 +4,20 @@
 from trytond.pool import Pool
 # from qualitycontrol import QualityControl
 from . import qualitycontrol
+from . import production
 __all__ = ['register']
 
 
 def register():
     Pool.register(
-        qualitycontrol.PostProductionCustomerSpecification,
-        qualitycontrol.PostProductionRejectedAnalysis,
-        qualitycontrol.PostProductionAnalysis,
-        qualitycontrol.PostProductionAnalysis1,
-        qualitycontrol.PreProductionLabCritarea1,
-        qualitycontrol.PreProductionCustomerSpecification,
-        qualitycontrol.PreProductionRejectedAnalysis,
         qualitycontrol.PreProductionLabCritarea,
         qualitycontrol.QualityControlPreproduction,
         qualitycontrol.QualityControlPostproduction,
+        production.QualityControlProduction,
+        production.QualityShipmentIn,
+          production.ProdShipment,
+        production.TemperatureAnalysisRecord,
+        production.MaterialBalanceSummary,
         module='quality_control', type_='model')
     Pool.register(
         module='quality_control', type_='wizard')
